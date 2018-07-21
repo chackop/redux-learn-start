1bd287e9ccdd07eec6e5c12430f060b237f38bbf
Chacko <chackop@gmail.com>
2018-07-21 16:01:03 +0100

    constants

--- src/reducers/countReducer.js
+++ src/reducers/countReducer.js
@@ -1,12 +1,15 @@
+import { SET_COUNTER, INCREMENT_COUNTER, DECREMENT_COUNTER, CLEAR_COUNTER } from './types';
+
 export default (state = 0, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case INCREMENT_COUNTER:
             return state + 1;
-        case 'DECREMENT':
+        // case 'DECREMENT':
+        case DECREMENT_COUNTER:
             return state - 1;
-        case 'CLEAR':
+        case CLEAR_COUNTER:
             return 0;
-        case 'SET':
+        case SET_COUNTER:
             return action.payload;
         default:
             return state;
